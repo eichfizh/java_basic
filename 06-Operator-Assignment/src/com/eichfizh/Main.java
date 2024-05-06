74f6a58054188b6435f154de26fcfe0a75be1ab9
Muhammad Hafizh <muhammaddhafizhh16@gmail.com>
2024-05-06 15:21:58 +0700

    06-Operator Assignment
    Operator Assignment operator penugasan yang digunakan untuk memasukkan nilai ke dalam suatu variabel contohnya +=, -=, %=

---
+++ 06-Operator-Assignment/src/com/eichfizh/Main.java
@@ -0,0 +1,35 @@
+package com.eichfizh;
+
+public class Main {
+    public static void main(String[] args) {
+        // Operator Assignment (=)
+
+        // Assignment operator jumlah
+        int a = 1;
+        // Normal Aritmatika
+        // a = a + 5;
+
+        a += 10; // a = a + 10;
+        System.out.println("nilai a = " + a);
+
+        // Assignment operator pengurangan
+        int b = 100;
+        b -= 30; // b = b + 100;
+        System.out.println("nilai b = " + b);
+
+        // Assignment operator perkalian
+        int c = 10;
+        c *= 30; // c = c * 30;
+        System.out.println("nilai c = " + c);
+
+        // Assignment operator pembagian
+        int d = 90;
+        d /= 30; // d = d / 30;
+        System.out.println("nilai d = " + d);
+
+        // Assignment operator modulus
+        int e = 100;
+        e %= 3; // e = e % 100;
+        System.out.println("nilai e = " + e);
+    }
+}
