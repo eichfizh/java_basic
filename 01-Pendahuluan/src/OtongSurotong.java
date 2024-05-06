de2d70dbba951cb2b5647290f0469dbe376d0a4d
Muhammad Hafizh <muhammaddhafizhh16@gmail.com>
2024-05-06 15:09:01 +0700

    01-Pendahuluan
    Memperkenalkan struktur java secara umum serta menjalankannya

---
+++ 01-Pendahuluan/src/OtongSurotong.java
@@ -0,0 +1,12 @@
+package src;
+class OtongSurotong {
+    public static void main(String[] args) {
+        // println digunakan untuk enter
+        System.out.println("hello world");
+        System.out.print("hello Juga");
+        // \n digunakan untuk enter tapi di dalam kutip
+        System.out.print(" \nhello Banget");
+        // String format
+        System.out.printf("Wiro Salto %d",212);
+    }
+}
---
+++ 01-Pendahuluan/src/com/eichfizh/Main.java
@@ -0,0 +1,10 @@
+package src.com.eichfizh;
+class Main {
+    // String[] string yang memiliki array dan args adalah tipe datanya adalah string
+    // String[] args intinya adalah menangkap sesuatu dibelakang run java main kita
+    public static void main(String[] args) 
+    {   
+        System.out.println(args[0]);
+        System.out.print("hello world");
+    }
+}
