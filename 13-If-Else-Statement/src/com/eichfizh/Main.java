8a11467e470804164a0a83880499ed90c82f6726
Muhammad Hafizh <muhammaddhafizhh16@gmail.com>
2024-05-07 10:55:59 +0700

    13-If Else Statement
    Mempelajari prinsip percabangan if else

---
+++ 13-If-Else-Statement/src/com/eichfizh/Main.java
@@ -0,0 +1,35 @@
+package com.eichfizh;
+
+public class Main {
+    public static void main(String[] args) {
+        // Contoh penggunaan if
+        int a = 5;
+
+        System.out.println("Nilai = " + a);
+
+        /*
+         * Ini adalah contoh if
+         * if (ekspresi){
+         *   statement
+         * }
+        */
+
+        /*
+         * Ini adalah contoh if-else
+         * if (ekspresi){
+         *   statement
+         * } else {
+         *   statement
+         * }
+        */
+
+        // ini contoh percabangannya
+        if (a == 10) {
+            System.out.println("Ini jalur true");
+        } else {
+            System.out.println("Ini adalah jalur false");
+        }
+
+        System.out.println("Selesai");
+    }
+}
